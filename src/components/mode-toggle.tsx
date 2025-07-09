25e72a21d4bf54889d8a8764e36b893716123f28
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-09 12:34:23 +0530

    fix: resolved the hydration issues which occured during theme change

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,16 +1,19 @@
 "use client"
 
-import { useId, useState } from "react"
+import { useEffect, useId, useState } from "react"
 import { MoonIcon, SunIcon } from "lucide-react"
-
-import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
 import { useTheme } from "next-themes"
 
 export default function ModeToggle() {
   const id = useId()
-  const [checked, setChecked] = useState<boolean>(true)
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
+  const [checked, setChecked] = useState<boolean>(false);
+
+  useEffect(() => {
+    theme === "dark" && setChecked(true)
+  },[]);
+
   const themeChange = () => {
     if(checked) {
         setTheme("light");
@@ -27,18 +30,15 @@ export default function ModeToggle() {
           id={id}
           checked={checked}
           onCheckedChange={themeChange}
-          className="peer data-[state=checked]:bg-input/50 data-[state=unchecked]:bg-input/50 absolute inset-0 h-[inherit] w-auto [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-full [&_span]:data-[state=checked]:rtl:-translate-x-full"
+          className="peer data-[state=checked]:bg-input/50 data-[state=unchecked]:bg-muted absolute inset-0 h-[inherit] w-auto [&_span]:h-full [&_span]:w-1/2 [&_span]:transition-transform [&_span]:duration-300 [&_span]:ease-[cubic-bezier(0.16,1,0.3,1)] [&_span]:data-[state=checked]:translate-x-full [&_span]:data-[state=checked]:rtl:-translate-x-full"
         />
-        <span className="peer-data-[state=checked]:text-muted-foreground/70 pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center">
+        <span className="peer-data-[state=checked]:text-muted-foreground pointer-events-none relative ms-0.5 flex min-w-8 items-center justify-center text-center">
             <SunIcon size={16} aria-hidden="true" />
         </span>
         <span className="peer-data-[state=unchecked]:text-muted-foreground/70 pointer-events-none relative me-0.5 flex min-w-8 items-center justify-center text-center">
             <MoonIcon size={16} aria-hidden="true" />
         </span>
       </div>
-      <Label htmlFor={id} className="sr-only">
-        Labeled switch
-      </Label>
     </div>
   )
 }
