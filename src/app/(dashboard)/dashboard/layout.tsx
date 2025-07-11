a97b47eb2368ab88561b5a0fdc31a856cfc382f4
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-11 18:03:08 +0530

    feat: created layout field for all dashboard routes and also added sidebar in it

---
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+import LayoutHeader from "@/components/dashboard/layout-header";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+export default function SubLayout({
+  children,
+}: {
+  children: Readonly<ReactNode>;
+}) {
+  return (
+    <SidebarProvider defaultOpen={false}>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <LayoutHeader />
+          </div>
+        </header>
+        <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
