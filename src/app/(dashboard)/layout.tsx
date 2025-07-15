9e3bb35da37ed3d4fbf52175cf0860e1f9a751bb
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-15 22:50:16 +0530

    feat: implement user session validation and redirection in DashboardLayout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,11 +1,17 @@
 
+import { authUser } from "@/actions/authentication";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: Readonly<ReactNode>;
 }) {
+
+  const userSession = await authUser();
+  if(!userSession) redirect('/login');
+
   return (
     <>
     { children }
