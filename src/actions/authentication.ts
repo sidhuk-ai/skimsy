eb509cc74af562c532d779202b01d4dc3725a205
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-16 13:28:30 +0530

    refactor: remove unused user registration and authentication functions

--- src/actions/authentication.ts
+++ src/actions/authentication.ts
@@ -3,19 +3,6 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
-export async function registerUser(formData: FormData) {
-    const firstName = formData.get("firstName") as string;
-    const lastName = formData.get("lastName") as string;
-    const email = formData.get("email") as string;
-
-    
-}
-
-export async function authenticateUser(formData: FormData) {
-    const email = formData.get("email") as string;
-    const password = formData.get("pwd") as string;
-}
-
 export async function authUser() {
     const session = await auth.api.getSession({
         headers: await headers()
