91add50b7afc4ff9b4104892f26735faa353ee2d
sidhuk-ai <77151100+sidhuk-ai@users.noreply.github.com>
2025-07-18 23:41:44 +0530

    fix: fixed a small bug which issued while hitting /sign-up route

--- src/app/(authentication)/sign-up/page.tsx
+++ src/app/(authentication)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 
 export default async function Page() {
     const session = await authUser();
-    if(!session) redirect("/dashboard");
+    if(session) redirect("/dashboard");
 
     return (
         <>
